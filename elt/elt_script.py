0d1c8bdb024b2b7c9383f7046a95bfa42061bf79
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2025-01-26 00:10:07 +0300

    feat: initialised the script with a waiting for PostgreSQL connection function

---
+++ elt/elt_script.py
@@ -0,0 +1,19 @@
+import subprocess #controls inputs, outputs, etc.
+import time
+
+def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            result = subprocess.run(
+                ["pg_isready", "-h", host], check=True, capture_output=True, text=True)
+            if "accepting connections" in result.stdout:
+                print("Successfully connected to PostgreSQL")
+                return True
+        except subprocess.CalledProcessError as e:
+            print(f"Error connecting to PostgreSQL: {e}")
+            retries += 1
+            print(f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})")
+            time.sleep(delay_seconds)
+        print("Max retries reached. Exiting")
+        return False
