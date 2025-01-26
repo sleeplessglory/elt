a99616441f28c2dac1b9456ba99ac537b25a7996
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2025-01-26 12:36:01 +0300

    feat: finished writing the ELT script

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -17,3 +17,48 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
             time.sleep(delay_seconds)
         print("Max retries reached. Exiting")
         return False
+if not wait_for_postgres(host="source_postgres"):
+    exit(1)
+
+print("Starting the ELT script...")
+
+source_config = {
+    'dbname': "source_db",
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'source_postgres'
+}
+
+destination_config = {
+    'dbname': "destination_db",
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'destination_postgres'
+}
+
+dump_command = [
+    'pg_dump',
+    '-h', source_config('host'),
+    '-u', source_config('user'),
+    '-d', source_config('dbname'),
+    '-f', 'data_dump.sql',
+    '-w'
+]
+
+subprocess_env = dict(PGPASSWORD=source_config['password']) #set the environment variable to avoid the password dump
+
+subprocess.run(dump_command, env=subprocess_env, check=True)
+
+load_command = [
+    'psql',
+    '-h', destination_config('host'),
+    '-u', destination_config('user'),
+    '-d', destination_config('dbname'),
+    '-a', '-f', 'data_dump.sql'
+]
+
+subprocess_env=dict(PGPASSWORD=destination_config['password'])
+
+subprocess.run(load_command, env=subprocess_env, check=True)
+
+print("Ending the ELT script...")
